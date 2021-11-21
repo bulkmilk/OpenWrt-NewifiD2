0111404ddf46d014d35d69d403dc7307b8a56964
bulkmilk <bulkmilk@qq.com>
2021-11-21 22:25:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,8 +11,14 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.10.254/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.10.254/g' package/base-files/files/bin/config_generate
 
 sed -i 's/100/101/g' package/network/services/dnsmasq/files/dhcp.conf
 sed -i 's/150/100/g' package/network/services/dnsmasq/files/dhcp.conf
 sed -i 's/12h/24h/g' package/network/services/dnsmasq/files/dhcp.conf
+
+sed -i "2i uci set network.lan.ipaddr='192.168.10.254'" package/lean/default-settings/files/zzz-default-settings
+sed -i "3i uci set network.lan.netmask='255.255.255.0'" package/lean/default-settings/files/zzz-default-settings    
+sed -i "4i uci set network.lan.gateway='192.168.10.254'" package/lean/default-settings/files/zzz-default-settings
+sed -i "5i uci set network.lan.dns='119.29.29.29 182.254.116.116'" package/lean/default-settings/files/zzz-default-settings
+sed -i "6i uci commit network\n" package/lean/default-settings/files/zzz-default-settings
