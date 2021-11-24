c6ea366cec5958449a5a58f9498b1d91d83f70cc
bulkmilk <bulkmilk@qq.com>
2021-11-24 23:03:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,8 +25,9 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 sed -i "2i uci set network.lan.ipaddr='192.168.10.254'" package/lean/default-settings/files/zzz-default-settings
 sed -i "3i uci set network.lan.netmask='255.255.255.0'" package/lean/default-settings/files/zzz-default-settings    
 sed -i "4i uci set network.lan.gateway='192.168.10.254'" package/lean/default-settings/files/zzz-default-settings
-sed -i "5i uci set network.lan.dns='119.29.29.29 182.254.116.116'" package/lean/default-settings/files/zzz-default-settings
-sed -i "6i uci commit network\n" package/lean/default-settings/files/zzz-default-settings
+sed -i "5i uci set network.lan.gateway='192.168.10.254'" package/lean/default-settings/files/zzz-default-settings
+sed -i "6i uci set network.lan.delegate='0'" package/lean/default-settings/files/zzz-default-settings
+sed -i "7i uci commit network\n" package/lean/default-settings/files/zzz-default-settings
 
 # 安装最新luci-theme-argon主题
 # https://github.com/jerrykuku/luci-theme-argon
